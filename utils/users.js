3c135a62ecaf7414ebfdc911b2eda03d70be1240
Muhammad Usama <underusama@gmail.com>
2022-09-23 12:17:00 +0500

    remove user function

--- utils/users.js
+++ utils/users.js
@@ -16,9 +16,15 @@ function getCurrentUser(id) {
 
 // User leaves chat
 function userLeave(id) {
-  const user = users.find(user => user.id === id)
+  const index = users.findIndex(user => user.id === id)
+  if (index !== -1) {
+    return users.splice(index, 1)[0]
+  }
+}
+
+// Remove user
+function removeUser(id) {
   users = user.filter(user => user.id !== id)
-  return user
 }
 
 // Get room users
@@ -31,4 +37,5 @@ module.exports = {
   getCurrentUser,
   userLeave,
   getRoomUsers,
+  removeUser
 }
